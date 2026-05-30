dcc1aefd9189db6137d059b805bb5601b2d4442e
shivamani18 <shivamanibhukya1618@gmail.com>
2026-05-30 09:53:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assignment3.html","./assignment1-2.html","./assignment2.html","./assignment4.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
